d38249cfc49983f3f15c4bf544929ffcccebffde
liort <liortzuk@gmail.com>
2025-08-23 19:30:47 +0300

    pre version 0.0.15 add switch case things but agam wants it to say it works now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/camera/visionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/camera/visionPipeline.java
@@ -50,6 +50,7 @@ public class visionPipeline implements VisionProcessor {
         Mat mat0 = new Mat();
         Mat mat1 = new Mat();
         Mat mat2 = new Mat();
+        state = 0;
 
         // mat_i is the input for case i, mat i+1 is output
         switch (state){
@@ -58,15 +59,14 @@ public class visionPipeline implements VisionProcessor {
                 Imgproc.cvtColor(frame,mat0,Imgproc.COLOR_RGB2HSV);
                 Core.inRange(mat0,min,max,mat1);
                 state = 1;
-                break;
             case 1:
                 // make previous whites the initial color
                 Imgproc.cvtColor(frame, frame,Imgproc.COLOR_RGB2HSV);
                 Core.bitwise_and(frame, frame,mat2,mat1); // frame && frame == frame, but on it you use the mask
                 state = 2;
-                break;
             default:
                 Imgproc.cvtColor(mat2,frame,Imgproc.COLOR_HSV2RGB);
+                break;
         }
 
         return null;
